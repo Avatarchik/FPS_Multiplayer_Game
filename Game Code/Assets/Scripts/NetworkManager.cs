4d3e6d788cb19d97274de4e14f91bdb640f126c4
Scott Schmitz <scottaschmitz@gmail.com>
2014-07-24 20:45:00 -0400

    Completion of tutorial 5 ( https://www.youtube.com/watch?v=oZ7TQjWhtEw )

---
+++ Game Code/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	public Camera standyCamera;
+	public SpawnSpot[] spawnSpots;
+
+	// Use this for initialization
+	void Start () {
+
+		spawnSpots = GameObject.FindObjectsOfType<SpawnSpot> ();
+
+		Connect ();
+	}
+
+	void Connect() {
+		PhotonNetwork.ConnectUsingSettings( "MultiFPS v1.0" );
+	}
+
+	void OnGUI () {
+		GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString ());
+	}
+
+	void OnJoinedLobby () {
+		PhotonNetwork.JoinRandomRoom ();
+	}
+
+	void OnPhotonJoinFailed () {
+		Debug.Log ("OnPhotonJoinFailed");
+	}
+
+	void OnPhotonRandomJoinFailed () {
+		Debug.Log ("OnPhotonRandomJoinFailed");
+		PhotonNetwork.CreateRoom (null);
+	}
+
+	void OnJoinedRoom () {
+		Debug.Log ("OnJoinedRoom");
+
+		SpawnMyPlayer ();
+	}
+
+	void SpawnMyPlayer () {
+
+		if (spawnSpots == null) {
+			Debug.Log ("We dont have any spawn spots!!");
+			return;
+		}
+
+		SpawnSpot mySpawnSpot = spawnSpots [Random.Range (0, spawnSpots.Length)];
+
+		GameObject myPlayerGameObject = PhotonNetwork.Instantiate("PlayerController",  mySpawnSpot.transform.position, mySpawnSpot.transform.rotation, 0);
+
+		((MonoBehaviour)myPlayerGameObject.GetComponent("FPSInputController")).enabled = true;;
+		((MonoBehaviour)myPlayerGameObject.GetComponent("MouseLook")).enabled = true;;
+		myPlayerGameObject.transform.FindChild ("Main Camera").gameObject.SetActive(true);
+
+
+		standyCamera.enabled = false;
+	}
+}
