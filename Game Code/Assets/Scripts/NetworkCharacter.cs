2a007f0af458be57fa0b8c49e1a55ca64aa9ad72
Scott Schmitz <scottaschmitz@gmail.com>
2014-07-27 20:28:06 -0400

    Completion of tutorial 10

--- Game Code/Assets/Scripts/NetworkCharacter.cs
+++ Game Code/Assets/Scripts/NetworkCharacter.cs
@@ -6,9 +6,15 @@ public class NetworkCharacter : Photon.MonoBehaviour {
 	Vector3 realPosition = Vector3.zero;
 	Quaternion realRotation = Quaternion.identity;
 
+	Animator anim;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		anim = GetComponent<Animator> ();
+
+		if (anim == null) {
+			Debug.Log ("This character is missing an Animator component");
+		}
 	}
 	
 	// Update is called once per frame
@@ -26,12 +32,16 @@ public class NetworkCharacter : Photon.MonoBehaviour {
 			// this is our player, we must send out our actual position
 			aStream.SendNext(transform.position);
 			aStream.SendNext(transform.rotation);
+			aStream.SendNext(anim.GetFloat("Speed"));
+			aStream.SendNext(anim.GetBool("Jumping"));
 		}
 		else {
 			// this is someone elses player, we need to receive their player and update
 			// our version of that player
 			realPosition = (Vector3) aStream.ReceiveNext();
 			realRotation = (Quaternion) aStream.ReceiveNext();
+			anim.SetFloat("Speed", (float)aStream.ReceiveNext());
+			anim.SetBool("Jumping", (bool)aStream.ReceiveNext());
 		}
 	}
 
--- Game Code/Assets/Scripts/PlayerMovement.cs
+++ Game Code/Assets/Scripts/PlayerMovement.cs
@@ -10,26 +10,70 @@ public class PlayerMovement : MonoBehaviour {
 	 */
 
 	float speed = 10f;
-	Vector3 direction = Vector3.zero;
+	Vector3 direction = Vector3.zero; //forward/back and left/right
+
+	float jumpSpeed = 6f;
+	float verticalVelocity = 0f;
 
 	CharacterController cc;
-
+	Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		cc = GetComponent<CharacterController> ();
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log ("Updating the direction");
-		direction = transform.rotation * new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+
+		// WASD foward/back and left/right movement is stored in direction
+		direction = transform.rotation * new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+		if (direction.magnitude > 1f) {
+			direction = direction.normalized;
+		}
+
+		anim.SetFloat ("Speed", direction.magnitude);
+
+
+		if (cc.isGrounded && Input.GetButtonDown ("Jump")) {
+				verticalVelocity = jumpSpeed;
+		}
 	}
 
 	// Called once per physics loop
 	// Do all movement and physics stuff here
 	void FixedUpdate () {
-			Debug.Log ("Oh how you move me");
-			cc.SimpleMove (direction * speed);
+		Vector3 lDistance = direction * speed * Time.deltaTime;
+
+		if (cc.isGrounded && verticalVelocity < 0) {
+			anim.SetBool ("Jumping", false);
+			verticalVelocity = Physics.gravity.y * Time.deltaTime;
+		} 
+		else {
+			if(Mathf.Abs(verticalVelocity) > jumpSpeed * 0.75f) {
+				anim.SetBool("Jumping", true);
+			}
+		}
+
+		verticalVelocity += Physics.gravity.y * Time.deltaTime;
+
+		lDistance.y = verticalVelocity * Time.deltaTime;
+
+		cc.Move (lDistance);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
